d9e9738f260132fd786e2d7345c2db60daf31d8b
Macarena <hmacarena@gmail.com>
2018-11-10 16:58:47 -0300

    fish prefabs

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,23 +6,15 @@ using UnityEngine;
 public class GameManager : ScriptSingleton<GameManager>
 {
     #region Fish Sprites
-    [Header("Fish sprites")]
-    [SerializeField]
-    private Sprite shrimpSprite;
-
-    [SerializeField]
-    private Sprite crabSprite;
-
-    [SerializeField]
-    private Sprite salmonSprite;
-
-    [SerializeField]
-    private Sprite tunaSprite;
+    [Header("Fish prefab")]
+    public GameObject shrimpPrefab;
+    public GameObject crabPrefab;
+    public GameObject salmonPrefab;
+    public GameObject tunaPrefab;
     #endregion
 
     // Use this for initialization
     void Start () {
-		
 	}
 	
 	// Update is called once per frame
