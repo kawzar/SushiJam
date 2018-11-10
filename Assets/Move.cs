4b8af8323286d7515cc70e3561443c22b411c74b
Macarena <hmacarena@gmail.com>
2018-11-10 01:33:41 -0300

    adding scripts

---
+++ Assets/Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    [SerializeField] GameObject hand;
+    [SerializeField] GameObject nails;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.C))
+        {
+            nails.GetComponent<NailScript>().GrowNails();
+        }
+	}
+}
