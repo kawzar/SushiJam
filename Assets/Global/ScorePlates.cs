ec6bf7aa448923f77259403015b031144bfe80dc
Macarena <hmacarena@gmail.com>
2018-11-11 14:25:49 -0300

    Adding plates for Score UI

---
+++ Assets/Global/ScorePlates.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Global.Stats;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScorePlates : MonoBehaviour
+{
+
+    [SerializeField]
+    Image smallPlate;
+
+    [SerializeField]
+    Image mediumPlate;
+
+    [SerializeField]
+    Image largePlate;
+
+   public void ShowScorePlates()
+    {
+        var score = GameStats.Instance.GetScore();
+        
+        if(score < 15 * 50)
+        {
+            smallPlate.enabled = true;
+        }
+        else if (score >= 15 * 50 && score < 30 * 50)
+        {
+            mediumPlate.enabled = true;
+        }
+        else
+        {
+            largePlate.enabled = true;
+        }
+
+    }
+
+}
