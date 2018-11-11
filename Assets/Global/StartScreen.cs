a6cd4baabbd49a0f98b83fe7b92fecb254374b7a
Macarena <hmacarena@gmail.com>
2018-11-11 15:42:04 -0300

    Create start sscreen

---
+++ Assets/Global/StartScreen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using Global.Stats;
+
+public class Start : MyScreen
+{
+    public void ShowStartScreen()
+    {
+        Show();
+    }
+}
